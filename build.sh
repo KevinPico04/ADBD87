793275794cce479e1a40e95156bf1083f957542f
KevinPico04 <kevinprhd1@gmail.com>
2023-10-05 22:18:54 -0500

    change build

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+DJANGO_SUPERUSER_PASSWORD=picoreyes2003 python manage.py createsuperuser --no-input --username=kevinpico  --email=kevinprhd1@gmail.com
