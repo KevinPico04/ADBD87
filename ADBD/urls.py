e3091016daf38ff9d5e92c8e038e1a2ce151e20a
KevinPico04 <kevinprhd1@gmail.com>
2023-10-05 20:21:08 -0500

    change

--- ADBD/urls.py
+++ ADBD/urls.py
@@ -23,7 +23,7 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='https://www.google.com/'),
+    path('', views.home, name='home'),
     path('blog/', include('blog.urls'))
 ]
 
